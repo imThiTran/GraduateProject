5ec524d14b4126f4806c7646567f3be80c1e55dc
imThiTran <tranvanthitk113@gmail.com>
2022-04-02 15:57:20 +0700

    load phim len trang chu

--- routes/site.js
+++ routes/site.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-
+var Film = require('../models/film');
+var Showtime = require('../models/showtime');
 
 var Category = require('../models/category');
-router.get("/",function(req,res){
-  Category.find({}, function(err,cats){
+router.get("/",async function(req,res){
+  Category.find({},async function(err,cats){
     var today=new Date();
     var timeDay = today.getTime();
     var daylength=24*60*60*1000;
@@ -15,12 +16,33 @@ router.get("/",function(req,res){
         var nextday = new Date(timeDay);
         dayArr.push(nextday);
       }
-    res.render('index',{
-      dayArrs:dayArr,
-      cats: cats  
-    });
+      var filmArr=[];
+      const fi = await Film.find({});
+      const st = await Showtime.find({});
+        for (var i=0;i<fi.length;i++){
+          filmArr.push({
+            nameEN:fi[i].nameEN,
+            stArr:[]
+          })
+        }
+        for (var i=0;i<fi.length;i++){
+          for (var j=0;j<st.length;j++){
+            if (fi[i]._id== st[j].idFilm){
+              filmArr[i].stArr.push(st[j].timeStart);
+            } 
+          }
+        }
+          res.render('index',{
+            dayArrs:dayArr,
+            cats: cats,
+            filmArrs:filmArr
+          });
   })
   
+})
+
+router.get('/render/:time',(req,res)=>{
+
 })
 
 module.exports = router;
