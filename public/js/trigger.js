2b68509a58fd6f5363d031664ca3282107eb3e23
Nguyen Hang <hangnguyen.2000a@gmail.com>
2022-04-07 00:02:04 +0700

    Add css js quan ly user

--- public/js/trigger.js
+++ public/js/trigger.js
@@ -1,5 +1,20 @@
+//ADMIN
 //Add Suat chieu
 $('#btnAddSC').on('click', () => {
     var htmlObj = document.getElementById('form-addSC');
     htmlObj.innerHTML = htmlObj.innerHTML + '<div class="row lc-suatchieu"><div class="col-5 showTimeComponent"><label for="">Suất chiếu</label><select id="" name="" class="form-select"><option value="">9:00</option><option value="">12:00</option><option value="">20:00</option></select></div><div class="col-5 showTimeComponent"><label for="">Rạp</label><select id="room" name="room" class="form-select"><option value="1">CINEMA 1</option><option value="2">CINEMA 2</option><option value="3">CINEMA 3</option></select></div><div class="col-2"><button class="btnDelSC"> <i class="fa fa-times" aria-hidden="true"></i></button></div></div>';
 })
+//Block the user
+window.onload = disableSelect();
+
+function disableSelect() {
+    const selectPcs = document.querySelectorAll('.sl-active');
+    selectPcs.forEach(element => {
+        element && (element.disabled = true);
+    });
+}
+
+function toggleSelect(e) {
+    const selectPc = e.parentElement.parentElement.querySelector('.sl-active');
+    selectPc && (selectPc.disabled = e.checked);
+}
