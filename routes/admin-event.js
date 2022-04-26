f5350cb09c3277cf6d71a9de25ee4f90b3e21aef
unknown <nhoznhatk5@gmail.com>
2022-04-26 21:33:28 +0700

    event hmmmmmm

--- routes/admin-event.js
+++ routes/admin-event.js
@@ -1,7 +1,9 @@
 var express= require('express');
 var router=express.Router();
-
-
+var Event = require('../models/event')
+var User = require('../models/user');
+var cloudinary = require('cloudinary').v2;
+var fs = require('fs');
 //loai bo khoang trang trong chuoi
 function cleanText(text){
     return text.replace(/\s+/g,' ').trim();
@@ -11,5 +13,33 @@ router.get('/',(req,res)=>{
     res.render('admin/admin-event')
 })
 
+router.post('/add',(req,res)=>{
+    var {content,title} = req.body
+    var slug = (cleanText(title).replace(/\s/g, '-')).toLowerCase();
+    var photoFile;
+    if (req.files != null) photoFile=req.files.photo;
+    else photoFile="";
+    if (photoFile != "") { 
+        cloudinary.uploader.upload(photoFile.tempFilePath, { folder: "cinema/events/" + slug }, function (err, rsPhoto) {
+            if (err) throw err;
+            fs.unlink(photoFile.tempFilePath, function (err) {
+                if (err) throw err;
+            })
+            User.findOne({email:req.session.user},function(err,user){
+                var event = new Event({
+                    title:title,
+                    slug:slug,
+                    content:content,
+                    photo: rsPhoto.url,
+                    photoDrop: rsPhoto.public_id,
+                    author:user.fullname
+                })                
+                event.save(function (err) {
+                    if (err) throw err;                                                        
+                });
+            })            
+        })
+    }    
+})
 
 module.exports= router;
