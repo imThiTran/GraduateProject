e0bef4cc60b28c226d063a13693890df319731d3
imThiTran <tranvanthitk113@gmail.com>
2022-04-27 09:55:59 +0700

    validate login register

--- public/js/validate-thitran.js
+++ public/js/validate-thitran.js
@@ -1,29 +1,72 @@
-function isRequired(elements,methodd){
-    var spanerror=elements.parent().find('.span-error-'+methodd);
-    elements.blur(function(e){
-        if (elements.val().trim()=="") spanerror.text('Vui lòng nhập trường này');
+function isRequired(elements, methodd) {
+    var spanerror = elements.parent().find('.span-error-' + methodd);
+    elements.blur(function (e) {
+        if (elements.val().trim() == "") spanerror.text('Vui lòng nhập trường này');
     })
-    elements.on('input',function(e){
+    elements.on('input', function (e) {
         spanerror.html(null);
     })
 }
 
-function isNumberLength(elements,methodd){
-    var spanerror=elements.parent().find('.span-error-'+methodd);
-    elements.blur(function(e){
-        if (elements.val()<30 || elements.val()>1000) spanerror.text('Thời lượng phim không phù hợp');
+function isNumberLength(elements, methodd,minlength,maxlength,mess) {
+    var spanerror = elements.parent().find('.span-error-' + methodd);
+    elements.blur(function (e) {
+        if (elements.val() < minlength || elements.val() > maxlength) spanerror.text(mess);
     })
-    elements.on('input',function(e){
+    elements.on('input', function (e) {
         spanerror.html(null);
     })
 }
 
-function checkform(array,methodd,mes){
-    for (var i=0;i<array.length;i++)
-    {
-        if (array[i].val().trim()==""){
-            var spanerror=array[i].parent().find('.span-error-'+methodd);
+function checkform(array, methodd, mes) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].val().trim() == "") {
+            var spanerror = array[i].parent().find('.span-error-' + methodd);
             spanerror.text(mes);
         }
     }
 }
+
+function checkRegex(value,regex) {
+    if (value.match(regex)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isGmail(elements,methodd){
+    var spanerror = elements.parent().find('.span-error-' + methodd);
+    elements.blur(function (e) {
+        var regex=/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/;
+        if (checkRegex(elements.val(),regex)==false ) spanerror.text('Vui lòng nhập đúng định dạng gmail');
+    })
+    elements.on('input', function (e) {
+        spanerror.html(null);
+    })
+}
+
+function isName(elements,methodd){
+    var spanerror = elements.parent().find('.span-error-' + methodd);
+    elements.blur(function (e) {
+        var regex=/^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
+        if (checkRegex(elements.val(),regex)==false ) spanerror.text('Vui lòng nhập đúng tên');
+    })
+    elements.on('input', function (e) {
+        spanerror.html(null);
+    })
+}
+
+function isPhone(elements,methodd){
+    var spanerror = elements.parent().find('.span-error-' + methodd);
+    elements.blur(function (e) {
+        var regex=/(84|0[3|5|7|8|9])+([0-9]{8})\b/;
+        if (checkRegex(elements.val(),regex)==false ) spanerror.text('Vui lòng nhập đúng số điện thoại');
+    })
+    elements.on('input', function (e) {
+        spanerror.html(null);
+    })
+}
+
+
+
