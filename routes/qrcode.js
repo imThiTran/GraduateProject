b8c125e2bc163f687e0d7d2d65e84210c1791414
imThiTran <tranvanthitk113@gmail.com>
2022-04-28 11:58:57 +0700

    scan expire bill

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -11,44 +11,54 @@ Category.find({}, function (err, categories) {
     cats = categories
 })
 
+function generateDate(date, time) {
+    var newDay = new Date(date);
+    var timeArr = time.split(':');
+    newDay.setHours(timeArr[0], timeArr[1]);
+    return newDay;
+};
 
 router.post('/scan-bill', (req, res) => {
     var qrcode = req.body.qrcode;
-    var time=new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',hour12:false }) ;
-    var date=new Date().toLocaleDateString('en-GB'); 
+    var time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+    var date = new Date().toLocaleDateString('en-GB');
     Bill.findById(qrcode, (err, bi) => {
         if (bi) {
             if (bi.checkin == 0) {
                 Showtime.findById(bi.ticket[0].idShowtime, (err, st) => {
-                    Film.findById(st.idFilm, (err, fi) => {
-                        Room.findById(st.idRoom, (err, ro) => {
-                            var tickets='',snacks='';
-                            var timeStartArr=st.timeStart.split(':');
-                            st.timeStart= st.timeStart + ((timeStartArr[0]<12)?(' AM'):(' PM'));
-                            bi.ticket.forEach(function(tk,index){
-                                if (index==bi.ticket.length-1) tickets=tickets+tk.name;
-                                 else tickets=tickets+ tk.name+', '
-                            })
-                            if (bi.snack.length!=0){
-                                bi.snack.forEach(function(sn,index){
-                                    if (index==bi.ticket.length-1) snacks=snacks+ sn.quantity+' x '+sn.name;
-                                     else snacks=snacks+ sn.quantity+' x '+sn.name+', '
+                    var dateSt = generateDate(st.date, st.timeStart);
+                    if ((dateSt.getTime() + 1800000) < (new Date()).getTime()) res.send('Suất chiếu này đã hết hạn');
+                    else {
+                        Film.findById(st.idFilm, (err, fi) => {
+                            Room.findById(st.idRoom, (err, ro) => {
+                                var tickets = '', snacks = '';
+                                var timeStartArr = st.timeStart.split(':');
+                                st.timeStart = st.timeStart + ((timeStartArr[0] < 12) ? (' AM') : (' PM'));
+                                bi.ticket.forEach(function (tk, index) {
+                                    if (index == bi.ticket.length - 1) tickets = tickets + tk.name;
+                                    else tickets = tickets + tk.name + ', '
                                 })
-                            }
-                            res.send({
-                                id: bi._id,
-                                timePrint: date+' '+time,
-                                fullname: bi.fullname,
-                                nameEN: fi.nameEN,
-                                nameVN: fi.nameVN,
-                                date: new Date(st.date).toLocaleDateString('en-GB'),
-                                time: st.timeStart,
-                                room: ro.name,
-                                ticket: tickets,
-                                snack: snacks
-                            });
+                                if (bi.snack.length != 0) {
+                                    bi.snack.forEach(function (sn, index) {
+                                        if (index == bi.snack.length - 1) snacks = snacks + sn.quantity + ' x ' + sn.name;
+                                        else snacks = snacks + sn.quantity + ' x ' + sn.name + ', '
+                                    })
+                                }
+                                res.send({
+                                    id: bi._id,
+                                    timePrint: date + ' ' + time,
+                                    fullname: bi.fullname,
+                                    nameEN: fi.nameEN,
+                                    nameVN: fi.nameVN,
+                                    date: new Date(st.date).toLocaleDateString('en-GB'),
+                                    time: st.timeStart,
+                                    room: ro.name,
+                                    ticket: tickets,
+                                    snack: snacks
+                                });
+                            })
                         })
-                    })
+                    }
                 })
             }
             else {
@@ -67,11 +77,11 @@ router.get('/scan-qrcode', (req, res) => {
     });
 })
 
-router.post('/check-in',(req,res)=>{
-    var idBill=req.body.idBill;
-    Bill.findById(idBill,(err,bi)=>{
-        bi.checkin=1;
-        bi.save(function(err){
+router.post('/check-in', (req, res) => {
+    var idBill = req.body.idBill;
+    Bill.findById(idBill, (err, bi) => {
+        bi.checkin = 1;
+        bi.save(function (err) {
             if (err) throw err;
             res.send('success')
         })
