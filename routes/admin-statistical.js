89fc1548099b0f2c1d5153e50234ed655653e6eb
unknown <nhoznhatk5@gmail.com>
2022-05-02 12:04:15 +0700

    thong ke

--- routes/admin-statistical.js
+++ routes/admin-statistical.js
@@ -16,12 +16,8 @@ Showtime.find({}, function(err,showtimes){
 })
 
 var films=[]
-Film.find({}, function(err,fis){
+Film.find({"status": {"$in" : ['Đang khởi chiếu','Đã chiếu xong']}}, function(err,fis){
     films=fis
-})
-
-router.get('/',(req,res) => {
-    res.render('admin/admin-statistical')
 })
 
 router.get('/day',(req,res)=> {
@@ -114,6 +110,7 @@ router.get('/film',(req,res)=> {
     var datafilm = new Array(films.length)
     var datafilmfull = new Array(films.length)
     var tk = new Array(films.length)
+    var listnamefilm=[]
     Bill.find({payment:'1'}, function(err,bills){        
         for(let i=0;i<datafilm.length;i++){   
             datafilm[i] =0
@@ -133,14 +130,101 @@ router.get('/film',(req,res)=> {
                     tk[i]++
                 }
             })
+            listnamefilm.push(films[i].nameEN)
         }
         res.render('admin/admin-statistical-film',{
             datafilm:datafilm,
             datafilmfull:datafilmfull,
             tk:tk,
-            films:films
+            films:films,
+            listnamefilm:listnamefilm
         })    
     })
    
+})
+
+router.post('/day',(req,res) =>{
+    var year = req.body.getmonth.slice(0,4)
+    var month = req.body.getmonth.slice(5,7)
+    month=parseInt(month)
+    var days = daysInMonth(month,year)
+    var datamonth = new Array(days)
+    var datamonthfull = new Array(days)
+    var tk1=new Array(days)
+    var tk2=new Array(days)
+    var newbill1 = []
+    Bill.find({payment:'1'}, function(err,bills){
+        bills.forEach(bill => {            
+            if((bill.timebooking.getMonth()+1)==month && (bill.timebooking.getFullYear())==year) newbill1.push(bill)
+        });
+        for(let i=0;i<datamonth.length;i++){            
+            datamonth[i]=0
+            datamonthfull[i]=0
+            tk1[i]=0
+            tk2[i]=0
+            newbill1.forEach(bill => {
+                if((bill.timebooking.getDate()-1)==i){                    
+                    datamonth[i]+=parseInt(bill.total)                   
+                    datamonthfull[i]+=parseInt(bill.totalbill)
+                    bill.ticket.forEach(ticket => {
+                        if(ticket.name.includes('K')){
+                            tk2[i]+=1
+                        }else{
+                            tk1[i]+=1
+                        }
+                    })
+                }
+            })
+        }
+        res.send({
+            datamonth:datamonth,
+            datamonthfull:datamonthfull,
+            tk1:tk1,
+            tk2:tk2,
+            days:days,
+            month:month,
+            year:year
+        })
+    })   
+})
+
+router.post('/month',(req,res) =>{
+    var year = req.body.getyear
+    var datayear = new Array(12)
+    var datayearfull = new Array(12)
+    var tk1=new Array(12)
+    var tk2=new Array(12)
+    var newbill1 = []
+    Bill.find({payment:'1'}, function(err,bills){
+        bills.forEach(bill => {            
+            if((bill.timebooking.getFullYear())==year) newbill1.push(bill)
+        });
+        for(let i=0;i<datayear.length;i++){            
+            datayear[i]=0
+            datayearfull[i]=0
+            tk1[i]=0
+            tk2[i]=0
+            newbill1.forEach(bill => {
+                if((bill.timebooking.getMonth())==i){                    
+                    datayear[i]+=parseInt(bill.total)                   
+                    datayearfull[i]+=parseInt(bill.totalbill)
+                    bill.ticket.forEach(ticket => {
+                        if(ticket.name.includes('K')){
+                            tk2[i]+=1
+                        }else{
+                            tk1[i]+=1
+                        }
+                    })
+                }
+            })
+        }
+        res.send({
+            datayear:datayear,
+            datayearfull:datayearfull,
+            tk1:tk1,
+            tk2:tk2,            
+            year:year
+        })
+    })   
 })
 module.exports= router;
